3c3648605f5ae0f81e15cc6a3591d9bad6a50efd
cph1234 <1533915305@qq.com>
2025-03-03 23:02:32 +0800

    3.3

--- miniprogram/pages/personal/my_homework/my_homework.js
+++ miniprogram/pages/personal/my_homework/my_homework.js
@@ -1,47 +1,95 @@
-// pages/personal/my_homework/my_homework.js
+// pages/personal/my_timeline/my_timeline.js
+const app = getApp()
+const db = wx.cloud.database()
+const _ = db.command
 Page({
   data: {
-    timeline: [
-      {
-        year: 2024,
-        entries: [
-          {
-            day: 26,
-            month: 11,
-            image: '/image/tree-1.png',
-            description: '这棵树的树皮呈灰褐色，布满裂纹，透露出一种沧桑感。树冠茂密，绿叶层层叠叠，为大地投下一片...'
-          },
-          // 其他月份数据...
-        ]
-      },
-      {
-        year: 2023,
-        entries: [
-          {
-            day: 11,
-            month: 12,
-            image: '/image/tree-2.png',
-            description: '这棵树的树皮呈灰褐色，布满裂纹，透露出一种沧桑感。树冠茂密，绿叶层层叠叠，为大地投下一片...'
-          },
-          {
-            day: 11,
-            month: 10,
-            image: '/image/tree-2.png',
-            description: '这棵树的树皮呈灰褐色，布满裂纹，透露出一种沧桑感。树冠茂密，绿叶层层叠叠，为大地投下一片...'
+    userInfo:{},
+    selectedYear:new Date().getFullYear(),
+    treeInfo: [],
+    year:[]
+  },
+  onLoad(){
+    let year=[]
+    for (let i = -10; i <= 10; i++) {
+      year.push(new Date().getFullYear() + i);
+    }
+    this.setData({
+      year:year
+    })
+    db.collection("userInfo").where({
+      _openid:app.globalData.user_openid
+    }).get().then(res=>{
+      this.setData({
+        userInfo:res.data[0]
+      })
+      console.log(res.data)
+      this.getData(new Date().getFullYear())
+    })
+    
+  },
+  async getData(y){
+    const currentYear = y
+    const nextYear = y + 1
+    const startDate = new Date(currentYear + '-01-01T00:00:00.000Z')
+    const endDate = new Date(nextYear + '-01-01T00:00:00.000Z')
+    console.log(startDate)
+    console.log(endDate)
+    let trees = await db.collection("treeInfo")
+    .where({
+        userId:this.data.userInfo._id,
+        createTime:_.and(
+          _.gte(startDate),
+          _.lt(endDate)
+        )
+    })
+    .get()
+    let treeInfo = trees.data
+    for(let item of treeInfo){
+      item.list=[]
+      let homework = await db.collection("homeworkInfo")
+      .where({
+          userId:this.data.userInfo._id,
+          create_time:_.and(
+            _.gte(startDate),
+            _.lt(endDate)
+          ),
+          treeType:item.treeType
+      }).orderBy('create_time', 'desc')
+      .get()
+      for(let iten of homework.data){
+        let info = {}
+        let create_time = new Date(iten.create_time)
+        info.month = create_time.getMonth() + 1;
+        info.day = create_time.getDate();
+        info.id = iten._id
+        for(let iteb of iten.details){
+          if(iteb.files.length!=0){
+            info.url = iteb.files[0]
+            break
           }
-        ]
-      },
-      {
-        year: 2022,
-        entries: [
-          {
-            day: 12,
-            month: 12,
-            image: '/image/tree-2.png',
-            description: '这棵树的树皮呈灰褐色，布满裂纹，透露出一种沧桑感。树冠茂密，绿叶层层叠叠，为大地投下一片...'
-          }
-        ]
+        }
+        item.list.push(info)
       }
-    ]
-  }
+    }
+    this.setData({
+      treeInfo:treeInfo
+    })
+  },
+  bindYearChange(e) {
+    console.log(e)
+    this.setData({
+      selectedYear: this.data.year[e.detail.value]
+    })
+    this.setData({
+      timeline:[]
+    })
+    this.getData(this.data.year[e.detail.value])
+  },
+  /**
+   * 上拉加载更多
+   */
+  onReachBottom: function() {
+    this.getData(this.data.selectedYear)
+  },
 })
